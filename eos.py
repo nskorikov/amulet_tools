6c233708197769400350c952a09b9898855d9fc8
Nikolay Skorikov <nskorikov@gmail.com>
2017-09-01 14:39:20 +0500

    Implemented new format of input dat file

--- eos.py
+++ eos.py
@@ -11,29 +11,34 @@ emass_si = 9.10938215e-31
 autime_si = 2.418884326505e-17
 # atomic pressure unit in GPa
 aupress_gpa = 1.e-9 * emass_si / (bohr_si * autime_si**2)
+# hartree in eV units
+ha2ev = 27.21138505
+# Bohr in \AA units
+ae2aa = 0.52917721092
+# Bohr^3 in \AA^3 units
+ae2aa3 = 0.148184711
 
 
 def main():
-    ha2ev = 27.21138505
-    ae2aa = 0.52917721092
-    ae2aa3 = 0.148184711
 
-    v, e = read_data('energy.dat')
+    v, e = read_data('energy1.dat')
     vols = np.array(v)
     energies = np.array(e)
 
     eos = birch_murnaghan
-    x0 = np.array([energies.mean(), 0.1, 0.1, vols.mean()])  # initial guess of parameters
+    x0 = np.array([energies.mean(), 1.1, 1.1, vols.mean()])  # initial guess of parameters
     plsq = leastsq(cost, x0, args=(energies, vols, eos))
     # print('Fitted parameters = {0}'.format(plsq[0]))
     V0 = plsq[0][3]
     B0 = plsq[0][1]
-    a0 = V0**(1/3) * ae2aa
-    print('V0       =  {:10.7f}'.format(V0))
+    a0 = V0**(1/3)
+
+    print('V0(AA^3)  =  {:10.7f}'.format(V0))
     print('a0       =  {:10.7f}'.format(a0))
-    print('E0       =  {:10.7f}'.format(plsq[0][0]))
+    print('E0(eV)   =  {:10.7f}'.format(plsq[0][0]))
     print('B0       =  ', B0)
-    print('B0(GPa)  =  ', B0 * aupress_gpa)
+    # print('B0(GPa)  =  ', B0 * aupress_gpa)
+    print('B0(Mbar) =  ', B0 * 1.6021765)
     print('B1       =  {:10.7f}'.format(plsq[0][2]))
 
     # eos = murnaghan
@@ -44,13 +49,19 @@ def main():
     plt.plot(vols, energies, 'ro')
 
     # plot the fitted curve on top
-    x = np.linspace(min(vols), max(vols), 50)
+    x = np.linspace(min(38.0,min(vols)), max(max(vols),50.0), 50)
     y = eos(plsq[0], x)
     plt.plot(x, y, 'k-')
-    plt.xlabel('Volume')
-    plt.ylabel('Energy')
-    plt.savefig('nonlinear-curve-fitting1.png')
+    plt.xlabel('Volume, AA^3')
+    plt.ylabel('Energy, eV')
+    plt.savefig('nonlinear-curve-fitting1.eps')
 
+    with open('fit.dat', 'w') as f:
+        for xi, yi in zip(x,y):
+            f.write('{0:8.4f}{1:8.4f}\n'.format(xi, yi))
+        f.write('\n\n')
+        for V,E in zip(vols, energies):
+            f.write('{0:8.4f}{1:8.4f}\n'.format(V, E))
 
 def murnaghan(parameters, vol):
     """From Phys. Rev. B 28, 5480 (1983)"""
@@ -74,15 +85,33 @@ def cost(param, y, x, eos):
 
 
 def read_data(fname):
+    a = []
     e = []
+    edft = []
+    edmft = []
+    de = []
     v = []
     with open(fname, 'r') as f:
         for line in f:
             if not re.match(r'^#', line):
                 tmp = line.split()
                 if len(tmp) > 1:
-                    v.append(float(tmp[0].strip()))
-                    e.append(float(tmp[1].strip()))
+                    a.append(float(tmp[0].strip()))
+                    v.append(float(tmp[1].strip()) * ae2aa3)
+                    edft.append(float(tmp[2].strip()) * ha2ev)
+                    edmft.append(float(tmp[3].strip()))
+                    de.append(float(tmp[4].strip()))
+    # TODO: add some automatic for this shift. But the shift should be
+    # conformed between different phases of one system.
+    # emin = np.min(edft)    # this break consistence between phases
+    # edft -= emin
+    for i in range(len(edft)):
+        edft[i] -= -138538.444909219
+    print('Zero of energy is shifted to -138538.444909219 eV')
+
+    for e1,e2 in zip(edft,edmft):
+        e.append(e1+e2)
+
     # for vv, ee in zip(V,E):
     #     print(vv,ee)
     return v, e
